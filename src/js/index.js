fc104e9e1658c6dd02c7d6e7341f6e91520fe17f
Martin <martin.setina@seznam.cz>
2020-02-07 11:49:17 +0100

    pacman as constructor

--- src/js/index.js
+++ src/js/index.js
@@ -1,91 +1,210 @@
-const pacman = document.querySelector('.entity--pac');
-let mouth = 'closed';
-let xpos = 100;
-let ypos = 100;
 const tileSize = 85;
 
-const update = () => {
-  pacman.style.left = String(xpos) + `px`;
-  pacman.style.top = String(ypos) + `px`;
-}
+class Pacman {
+  constructor(xposition, yposition, mouth){
+    this.xposition = xposition;
+    this.yposition = yposition;
+    this.mouth = mouth;
+    
+  }
 
+  moveRight() {
+    this.element.style.backgroundPositionX = '85px';
+    this.xposition += tileSize;
+  if (this.mouth === 'closed') {
+    this.element.style.backgroundPositionX = '0px';
+    this.element.style.backgroundPositionY = '0px';
+    this.mouth = 'open';
+  } else { 
+    this.element.style.backgroundPositionX = '85px';
+    this.element.style.backgroundPositionY = '0px';
+    this.mouth = 'closed';
+  }
+  }
 
-document.addEventListener('DOMContentLoaded', () => {
+  moveLeft() {
+    this.element.style.backgroundPositionX = '85px';
+    this.xposition -= tileSize;
+ 
+  if (this.mouth === 'open') {
+    this.element.style.backgroundPositionX = '0px';
+    this.element.style.backgroundPositionY = '255px';
+    this.mouth = 'closed';
+  } else { 
+    this.element.style.backgroundPositionX = '85px';
+    this.element.style.backgroundPositionY = '255px';
+    this.mouth = 'open';
+  }
 
-  update();
+  }
 
-
-
-  document.addEventListener('keydown', (event) => {
-    if(event.key === 'ArrowRight') {
-      xpos += tileSize;
-      pacman.style.left = String(xpos) + `px`;
-    if (mouth === 'closed') {
-      pacman.style.backgroundPositionX = '0px';
-      pacman.style.backgroundPositionY = '0px';
-      mouth = 'open';
+  moveDown() {
+    this.element.style.backgroundPositionX = '85px';
+    this.yposition += tileSize;
+    if (this.mouth === 'open') {
+      this.element.style.backgroundPositionX = '0px';
+      this.element.style.backgroundPositionY = '170px';
+      this.mouth = 'closed';
     } else { 
-      pacman.style.backgroundPositionX = '85px';
-      pacman.style.backgroundPositionY = '0px';
-      mouth = 'closed';
+      this.element.style.backgroundPositionX = '85px';
+      this.element.style.backgroundPositionY = '170px';
+      this.mouth = 'open';
+      }
+  }
+
+  moveUp() {
+    this.element.style.backgroundPositionX = '85px';
+    this.yposition -= tileSize;
+    if (this.mouth === 'open') {
+      this.element.style.backgroundPositionX = '0px';
+      this.element.style.backgroundPositionY = '85px';
+      this.mouth = 'closed';
+    } else { 
+      this.element.style.backgroundPositionX = '85px';
+      this.element.style.backgroundPositionY = '85px';
+      this.mouth = 'open';
     }
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'entity entity--pac pacboy-active-light';
+
+    document.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowRight') {
+      this.moveRight();
+      this.update();
     }
   });
 
   document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowLeft') {
-      pacman.style.backgroundPositionX = '85px';
-      xpos -= tileSize;
-      pacman.style.left = String(xpos) + `px`;
-   
-    if (mouth === 'open') {
-      pacman.style.backgroundPositionX = '0px';
-      pacman.style.backgroundPositionY = '255px';
-      mouth = 'closed';
-    } else { 
-      pacman.style.backgroundPositionX = '85px';
-      pacman.style.backgroundPositionY = '255px';
-      mouth = 'open';
-    }
+      this.moveLeft();
+      this.update();
     }
   });
 
 
   document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowUp') {
-      pacman.style.backgroundPositionX = '85px';
-      ypos -= tileSize;
-      pacman.style.top = String(ypos) + `px`;
-      if (mouth === 'open') {
-        pacman.style.backgroundPositionX = '0px';
-        pacman.style.backgroundPositionY = '85px';
-        mouth = 'closed';
-      } else { 
-        pacman.style.backgroundPositionX = '85px';
-        pacman.style.backgroundPositionY = '85px';
-        mouth = 'open';
-      }
-    }
+      this.moveUp();
+      this.update();
+     }
   });
 
   document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowDown') {
-      pacman.style.backgroundPositionX = '85px';
-      ypos += tileSize;
-      pacman.style.top = String(ypos) + `px`;
-      if (mouth === 'open') {
-        pacman.style.backgroundPositionX = '0px';
-        pacman.style.backgroundPositionY = '170px';
-        mouth = 'closed';
-      } else { 
-        pacman.style.backgroundPositionX = '85px';
-        pacman.style.backgroundPositionY = '170px';
-        mouth = 'open';
-      }
+      this.moveDown();
+      this.update();
     }
   });
+}
 
-});
+  update() {
+    this.element.style.left = String(this.xposition) + `px`;
+    this.element.style.top = String(this.yposition) + `px`;  
+  }
+
+  mount(parent) {
+    this.render();
+    this.update();
+    parent.appendChild(this.element);
+  }
+}
+
+const app = document.querySelector('#app');
+const pacman1 = new Pacman(100, 100, 'closed');
+pacman1.mount(app);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
+
+
+
+// const update = () => {
+//   pacman.style.left = String(xpos) + `px`;
+//   pacman.style.top = String(ypos) + `px`;
+// }
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//   update();
+
+
+  // document.addEventListener('keydown', (event) => {
+  //   if(event.key === 'ArrowRight') {
+  //     this.element.style.backgroundPositionX = '85px';
+  //     this.xposition += tileSize;
+  //     pacman.style.left = String(xpos) + `px`;
+  //   if (mouth === 'closed') {
+  //     pacman.style.backgroundPositionX = '0px';
+  //     pacman.style.backgroundPositionY = '0px';
+  //     mouth = 'open';
+  //   } else { 
+  //     pacman.style.backgroundPositionX = '85px';
+  //     pacman.style.backgroundPositionY = '0px';
+  //     mouth = 'closed';
+  //   }
+  //   }
+  // });
+
+  // document.addEventListener('keydown', (event) => {
+  //   if(event.key === 'ArrowLeft') {
+  //     pacman.style.backgroundPositionX = '85px';
+  //     xpos -= tileSize;
+  //     pacman.style.left = String(xpos) + `px`;
+   
+  //   if (mouth === 'open') {
+  //     pacman.style.backgroundPositionX = '0px';
+  //     pacman.style.backgroundPositionY = '255px';
+  //     mouth = 'closed';
+  //   } else { 
+  //     pacman.style.backgroundPositionX = '85px';
+  //     pacman.style.backgroundPositionY = '255px';
+  //     mouth = 'open';
+  //   }
+  //   }
+  // });
+
+
+  // document.addEventListener('keydown', (event) => {
+  //   if(event.key === 'ArrowUp') {
+  //     pacman.style.backgroundPositionX = '85px';
+  //     ypos -= tileSize;
+  //     pacman.style.top = String(ypos) + `px`;
+  //     if (mouth === 'open') {
+  //       pacman.style.backgroundPositionX = '0px';
+  //       pacman.style.backgroundPositionY = '85px';
+  //       mouth = 'closed';
+  //     } else { 
+  //       pacman.style.backgroundPositionX = '85px';
+  //       pacman.style.backgroundPositionY = '85px';
+  //       mouth = 'open';
+  //     }
+  //   }
+  // });
+
+  // document.addEventListener('keydown', (event) => {
+  //   if(event.key === 'ArrowDown') {
+  //     pacman.style.backgroundPositionX = '85px';
+  //     ypos += tileSize;
+  //     pacman.style.top = String(ypos) + `px`;
+  //     if (mouth === 'open') {
+  //       pacman.style.backgroundPositionX = '0px';
+  //       pacman.style.backgroundPositionY = '170px';
+  //       mouth = 'closed';
+  //     } else { 
+  //       pacman.style.backgroundPositionX = '85px';
+  //       pacman.style.backgroundPositionY = '170px';
+  //       mouth = 'open';
+  //     }
+  //   }
+  // });
+
+
+// });
 
 
 
